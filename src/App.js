95563c1334a81492747cb7685bdb4baa7d0895c2
WINDOWS-11\Administrator <theend.spirit@gmail.com>
2024-11-03 22:40:24 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Cao Trung Hiếu test</p>
+        <p>Dòng 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
